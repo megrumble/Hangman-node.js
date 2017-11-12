dd8e67f9fd2bac70ab4ff2e280a1d2b8863eba46
Margaret Rumble <megrumble@gmail.com>
2017-11-12 09:46:51 -0500

    inquirer

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log(HANGMAN);
+var inquirer = require("inquirer");
+
+function guessLetter() {
+    var questions = [{
+        name: 'letter',
+        type: 'text',
+        message: 'Guess a letter'
+    }]
+    inquirer.prompt(questions)
+        .then(function () {
+
+        })
+}
