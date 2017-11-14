753fb2a7bc9035724d2a2fa930681a0579e3e694
Margaret Rumble <megrumble@gmail.com>
2017-11-14 10:59:37 -0500

    word finished, working on main

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 console.log(HANGMAN);
 var inquirer = require("inquirer");
+var game = require("./game");
+var word = require('./word');
 
+//start game
+function startGame() {
+
+}
+//get word from wordArray
+function getWord() {
+    var currentWord = new word(this.wordArray[Math.floor(Math.random() * this.wordArray.length)]);
+}
+//display dashes for word
+//prompt user to guess a letter
+//check to see if letter is in word and display letter if correct
+//add letter to incorrectArray if incorrect
+//continue until word isComplete or...
+//all incorrect guesses are used 
 function guessLetter() {
     var questions = [{
         name: 'letter',
@@ -8,7 +24,7 @@ function guessLetter() {
         message: 'Guess a letter'
     }]
     inquirer.prompt(questions)
-        .then(function () {
+        .then(function (letter) {
 
         })
 }
--- word.js
+++ word.js
@@ -1,18 +1,30 @@
 var letter = require("/.letter");
 
-function word() {
+function word(value) {
     this.value = value;
     this.letters = [];
     this.found = false;
+    //string to hold letters guesssed
+    this.guesses = '';
     for (i = 0; i < this.value.length; i++) {
         this.letters.push(new letter(this.value[i]));
     }
 }
-//check to see if letter is in word
+//check to see if letter has been guessed using string position
 word.prototype.checkLetter = function () {
-
-}
-
+    var guessLetter = letter.toLowerCase();
+    //if the letter already exists anywhere in the guesses string...
+    if (this.guesses.indexOf(guessLetter) != -1) {
+        return console.log("You've already guessed this letter!");
+    }
+    //otherwise add guessed letter to guesses string
+    this.guesses += guessLetter;
+    for (i = 0; i < this.letters.length; i++) {
+        if (this.letters[i].value.toLowerCase() == guessLetter) {
+            this.letters[i].appear = true;
+        }
+    }
+};
 //check if word is complete
 word.prototype.isComplete = function () {
     for (var i = 0; i < this.letters.length; i++) {
@@ -21,7 +33,6 @@ word.prototype.isComplete = function () {
     }
     return this.found = true;
 }
-
 //display word
 word.prototype.displayWord = function () {
     var string = '';
@@ -30,3 +41,5 @@ word.prototype.displayWord = function () {
     }
     return string;
 }
+
+module.exports = word
